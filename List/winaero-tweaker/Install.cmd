c75d993f28be92a24e49f8d3ea817db7c1c9173a
MrR736 <mrr6451@outlook.com>
2024-08-30 17:27:52 -0700

    Delete List/winaero-tweaker/Install.cmd

--- List/winaero-tweaker/Install.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-for /f "tokens=*" %%f in ('dir /b /a-d "*.exe"') do (
-    @echo %%f
-    @rename "%%f" "Install.exe"
-)
-set SetupApp=Install.exe
-start %SetupApp% /SP- /VERYSILENT /PORTABLE /DIR="%root%\WinaeroTweaker"
-exit
