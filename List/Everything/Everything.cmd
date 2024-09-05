a8d7921ede446bb4df9176f447803dc4f40e2b95
MrR736 <mrr6451@outlook.com>
2024-09-04 18:02:15 -0700

    Update Everything.cmd

--- List/Everything/Everything.cmd
+++ List/Everything/Everything.cmd
@@ -5,4 +5,4 @@ for /f "tokens=*" %%f in ('dir /b /a-d "*.zip"') do (
     @echo %%f
     @rename "%%f" "Everything.zip"
 )
-7za.exe e Everything.zip -o%Core%\CoreUpdates\Temp\Everything\
+7za.exe e Everything.zip -o"%ProgramData%\PhoenixOS\Core\CoreUpdates\Temp\Everything"
