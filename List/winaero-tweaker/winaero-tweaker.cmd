cb60634d78f7031cba22775c321ed77db388463b
MrR736 <mrr6451@outlook.com>
2024-09-04 18:03:06 -0700

    Update winaero-tweaker.cmd

--- List/winaero-tweaker/winaero-tweaker.cmd
+++ List/winaero-tweaker/winaero-tweaker.cmd
@@ -5,4 +5,4 @@ for /f "tokens=*" %%f in ('dir /b /a-d "*.zip"') do (
     @echo %%f
     @rename "%%f" "winaerotweaker.zip"
 )
-7za.exe e winaerotweaker.zip -o%Core%\CoreUpdates\Temp\winaero-tweaker\
+7za.exe e winaerotweaker.zip -o"%ProgramData%\PhoenixOS\Core\CoreUpdates\Temp\winaero-tweaker"
