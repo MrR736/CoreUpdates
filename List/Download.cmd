f1cc865b0a5c0b54d366e1dd6c65772242eef696
MrR736 <mrr6451@outlook.com>
2024-09-04 18:16:14 -0700

    Add files via upload

--- List/Download.cmd
+++ List/Download.cmd
@@ -5,6 +5,4 @@ call "%cd%\Everything\Everything.cmd"
 cd /d %~dp0
 call "%cd%\winaero-tweaker\winaero-tweaker.cmd"
 cd /d %~dp0
-call "%cd%\winaero-tweaker\winaero-tweaker.cmd"
-cd /d %~dp0
 call "%cd%\Xd-AntiSpy\Xd-AntiSpy.cmd"
