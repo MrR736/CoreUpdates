b9fba688bd885a77e8265c1ebab64ad17c35a542
MrR736 <mrr6451@outlook.com>
2024-09-09 02:16:15 -0700

    Delete Install Manager directory

--- Install Manager/Install2.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-for /f "tokens=*" %%f in ('dir /b /a-d "*.exe"') do (
-    @echo %%f
-    @rename "%%f" "Search.exe"
-)
-copy "Search.exe" "%ProgramData%\PhoenixOS\Search"
-exit
