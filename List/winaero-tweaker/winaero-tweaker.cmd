e55d2189a50f7a285a9ab626a9ad6ea1bc8a2544
MrR736 <mrr6451@outlook.com>
2024-09-10 11:52:38 -0700

    Add files via upload

--- List/winaero-tweaker/winaero-tweaker.cmd
+++ List/winaero-tweaker/winaero-tweaker.cmd
@@ -1,6 +1,4 @@
 @echo off
 cd /d %~dp0
-wget -q -O winaerotweaker.cmd https://raw.githubusercontent.com/MrR736/CoreAbout/main/Downloads/winaerotweaker.cmd
-call winaerotweaker.cmd
-del /s /q ".\winaerotweaker.cmd"
+wget -q -O winaerotweaker.zip https://winaerotweaker.com/download/winaerotweaker.zip
 7za.exe e winaerotweaker.zip -o"%ProgramData%\PhoenixOS\Core\CoreUpdates\Temp\winaero-tweaker"
