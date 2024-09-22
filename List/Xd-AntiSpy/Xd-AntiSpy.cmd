bf2ff501cacf3fca5187ff49902a9943305ac84c
MrR736 <mrr6451@outlook.com>
2024-09-22 14:28:02 -0700

    Delete List directory

--- List/Xd-AntiSpy/Xd-AntiSpy.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-set /p p=<Path.txt
-taskKill /f /im xd-AntiSpy.exe /t
-del /s /q "%ProgramData%\PhoenixOS\Extra Tools\Xd-AntiSpy"
-start /w cmd.exe /c call "MakeUrl.bat"
-%p%\7z.exe e Xd-AntiSpy.zip -o"%ProgramData%\PhoenixOS\Extra Tools\Xd-AntiSpy\"
-del /s /q ".\Url\Path.txt"
--- List/winaero-tweaker/winaero-tweaker.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d %~dp0
-
-set /p P=<Path.txt
-
-%p%\wget.exe -q -O winaerotweaker.zip https://winaerotweaker.com/download/winaerotweaker.zip
-%p%\7z.exe e winaerotweaker.zip -o"%ProgramData%\PhoenixOS\Core\CoreUpdates\Temp\winaero-tweaker"
