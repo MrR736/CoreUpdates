41330a8b4afd11d57d63f6aa8bbdff1daa3e2f6c
MrR736 <mrr6451@outlook.com>
2024-09-22 14:28:10 -0700

    Delete Install Manager directory

--- Install Manager/Install.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d %~dp0
-for /f "tokens=*" %%f in ('dir /b /a-d "*.exe"') do (
-    @echo %%f
-    @rename "%%f" "Install.exe"
-)
-start /WAIT Install.exe /SP- /VERYSILENT /PORTABLE /DIR="%ProgramData%\PhoenixOS\WinaeroTweaker"
-exit
